367c6bd82f2adf59e9d4e846134b28d52daa69dd
FedericoSerini <federico.serini.se@pec.it>
2025-01-27 09:30:21 +0100

    add: values.yaml, removed: fake key

--- apps/homarr/homarr_helm.sh
+++ apps/homarr/homarr_helm.sh
@@ -3,11 +3,11 @@ helm repo add homarr-labs https://homarr-labs.github.io/charts/
 
 helm repo update
 
-helm install homarr homarr-labs/homarr --namespace homarr --create-namespace --set env.SECRET_ENCRYPTION_KEY='0865afaca68b245f2dadccdec87cec49ede1815d046eca046cdfbc15632882ca'
+helm install homarr homarr-labs/homarr --namespace homarr --create-namespace
 
 secret=$(<secret.txt)
 
-# Ephimeral !
+# Ephemeral !
 kubectl create secret generic db-secret \
 --from-literal=db-encryption-key="$secret" \
 --namespace homarr
